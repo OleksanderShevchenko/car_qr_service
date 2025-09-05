16f6050520f1db3bfdf9ee0b87b3c9d8ca1df4e8
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-05 20:50:22 +0300

    Add new router to main starting script. Translate description to en

--- src/car_qr_service/main.py
+++ src/car_qr_service/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
+from src.car_qr_service.users.router import router as users_router
+
 
 app = FastAPI(title="Car QR Service",
-              description="Сервіс для зв'язку з власником авта через QR код.",
+              description="Service to contact with car owner by means of QR code.",
               version="0.0.1")
-
+# include routers
+app.include_router(users_router)
 
 @app.get("/")
 def get_root():
@@ -12,3 +15,9 @@ def get_root():
     Starting root end point
     """
     return {"message": "Hello, QR Car Service!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True, port=8101)
