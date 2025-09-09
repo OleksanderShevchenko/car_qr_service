ae417975c19b69854b18e90f11db081282eb36e9
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-09 10:32:54 +0300

    Add endpoint to get access token

---
+++ src/car_qr_service/auth/router.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.car_qr_service.auth.utils import create_access_token
+from src.car_qr_service.auth.security import verify_password
+from src.car_qr_service.database.database import get_db_session
+from src.car_qr_service.users import crud
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: Annotated[AsyncSession, Depends(get_db_session)]):
+    """
+    Endpoint for getting JWT-token.
+    Gets email as `username` and password.
+    """
+    # 1. Знаходимо користувача за email (який передається в полі username)
+    user = await crud.get_user_by_email(form_data.username, db)
+
+    # 2. Якщо користувача не знайдено АБО пароль невірний, повертаємо помилку
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неправильний email або пароль",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # 3. Створюємо JWT-токен
+    access_token = create_access_token(data={"sub": user.email})
+
+    # 4. Повертаємо токен
+    return {"access_token": access_token, "token_type": "bearer"}
