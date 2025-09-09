7ef28dfc59b77c75ecc23d813858bd8c78df6c70
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-09 10:51:38 +0300

    Include new routers. Remove running from current dir - it will be inconsistent as far as database is placed in the root - outside src. It should be ron from the root like this: poetry run uvicorn src.car_qr_service.main:app --reload --port 8001

--- src/car_qr_service/main.py
+++ src/car_qr_service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.car_qr_service.users.router import router as users_router
+from src.car_qr_service.auth.router import router as login_user
 
 
 app = FastAPI(title="Car QR Service",
@@ -8,6 +9,8 @@ app = FastAPI(title="Car QR Service",
               version="0.0.1")
 # include routers
 app.include_router(users_router)
+app.include_router(login_user)
+
 
 @app.get("/")
 def get_root():
@@ -15,9 +18,3 @@ def get_root():
     Starting root end point
     """
     return {"message": "Hello, QR Car Service!"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", reload=True, port=8101)
