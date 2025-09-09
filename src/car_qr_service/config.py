bfbffc708398b9de110f1f116b5656131fe60edc
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-09 10:26:42 +0300

    Add settings for JSON Web Token - JWT

--- src/car_qr_service/config.py
+++ src/car_qr_service/config.py
@@ -21,6 +21,9 @@ class Settings(BaseSettings):
     """
     # URL для підключення до бази даних
     DB_URL: str = DB_URL
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = SettingsConfigDict(env_file=".env")
 
