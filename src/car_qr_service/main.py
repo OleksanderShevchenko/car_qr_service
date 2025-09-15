ab2dc6e6c553ceb12731181ecdc7ae72319b3417
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-15 20:24:43 +0300

    Connect new page router

--- src/car_qr_service/main.py
+++ src/car_qr_service/main.py
@@ -4,6 +4,7 @@ from src.car_qr_service.users.router import router as users_router
 from src.car_qr_service.auth.router import router as login_user
 from src.car_qr_service.cars.router import router as car_router
 from src.car_qr_service.public.router import router as public_router
+from src.car_qr_service.pages.router import router as pages_router
 
 
 app = FastAPI(title="Car QR Service",
@@ -14,6 +15,7 @@ app.include_router(users_router)
 app.include_router(login_user)
 app.include_router(car_router)
 app.include_router(public_router)
+app.include_router(pages_router)
 
 
 @app.get("/")
