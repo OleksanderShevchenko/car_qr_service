d0d0eeed4de45cd23ea4e42ad39c8f411b615f60
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-15 21:38:56 +0300

    Add new optional field in the model that means flag if the user allow to show the phone number

--- src/car_qr_service/database/models.py
+++ src/car_qr_service/database/models.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import String, ForeignKey, func, DateTime
+from sqlalchemy import String, ForeignKey, func, DateTime, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.car_qr_service.database.database import Base
@@ -10,6 +10,8 @@ class User(Base):
     """
     Модель Користувача.
     Представляє таблицю 'users' в базі даних.
+    User Model.
+    Represents the 'users' table in the database.
     """
     __tablename__ = "users"
 
@@ -22,9 +24,11 @@ class User(Base):
     created_at: Mapped[datetime.datetime] = mapped_column(
         DateTime, default=datetime.datetime.now(datetime.timezone.utc), server_default=func.now()
     )
-
+    show_phone_number: Mapped[bool] = mapped_column(Boolean, default=False, server_default="0")
     # Зв'язок "один-до-багатьох": один користувач може мати багато автомобілів.
     # back_populates="owner" вказує на атрибут 'owner' в моделі Car.
+    # One-to-many relationship: one user can have many cars.
+    # back_populates="owner" points to the 'owner' attribute in the Car model.
     cars: Mapped[list["Car"]] = relationship(back_populates="owner")
 
 
@@ -32,6 +36,8 @@ class Car(Base):
     """
     Модель Автомобіля.
     Представляє таблицю 'cars' в базі даних.
+    Car Model.
+    Represents the 'cars' table in the database.
     """
     __tablename__ = "cars"
 
@@ -40,7 +46,8 @@ class Car(Base):
     brand: Mapped[str] = mapped_column(String(50))
     model: Mapped[str] = mapped_column(String(50))
     owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-
     # Зв'язок "багато-до-одного": багато авто можуть належати одному користувачу.
     # back_populates="cars" вказує на атрибут 'cars' в моделі User.
+    # Many-to-one relationship: many cars can belong to the same user.
+    # back_populates="cars" points to the 'cars' attribute in the User model.
     owner: Mapped["User"] = relationship(back_populates="cars")
