c95e4799adf48195519fac4ff0b7c1338caf431c
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-09-23 21:16:00 +0300

    Optimize "login_for_access_token" function - reuse authenticate_user function from utils. In error message change email to username (which is actually email that serves as username)

--- src/car_qr_service/auth/router.py
+++ src/car_qr_service/auth/router.py
@@ -4,10 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.car_qr_service.auth.utils import create_access_token
-from src.car_qr_service.auth.security import verify_password
+from src.car_qr_service.auth.utils import create_access_token, authenticate_user
 from src.car_qr_service.database.database import get_db_session
-from src.car_qr_service.users import crud
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -23,23 +21,13 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     Endpoint for getting JWT-token.
     Gets email as `username` and password.
     """
-    # 1. Знаходимо користувача за email (який передається в полі username)
-    #    Find the user by email (which is passed in the username field)
-    user = await crud.get_user_by_email(form_data.username, db)
-
-    # 2. Якщо користувача не знайдено АБО пароль невірний, повертаємо помилку
-    #    If the user is not found OR the password is incorrect, we return an error
-    if not user or not verify_password(form_data.password, user.hashed_password):
+    # Використовуємо нашу нову централізовану функцію
+    user = await authenticate_user(form_data.username, form_data.password, db)
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-
-    # 3. Створюємо JWT-токен
-    #    Creating a JWT token
     access_token = create_access_token(data={"sub": user.email})
-
-    # 4. Повертаємо токен
-    #    Returning the token
     return {"access_token": access_token, "token_type": "bearer"}
